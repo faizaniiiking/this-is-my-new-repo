8598469fa9c0dcaa6e2e5e89b953a572128336d6
faizan khan <faizanikkhan@gmail.com>
2024-10-21 18:17:17 +0530

    added some files

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    console.log("i am man")
+    const buttun = document.getElementById("btn")
+    buttun.style.backgroundColor = "red"
+    buttun.style.color = "white"
+
+}
